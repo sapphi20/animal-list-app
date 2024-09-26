10922dcfe074de6a6cad663c362f7f932d6a7aa5
Alejandra Alarcón <sapphi20@gmail.com>
2024-09-26 00:21:37 -0300

    :sparkles: Add recycler view for animal item list

---
+++ app/src/main/java/com/example/animallist/AnimalListAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.animallist
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AnimalListAdapter(private val list: List<String>) : RecyclerView.Adapter<AnimalListAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.animal_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = list[position]
+        holder.itemView.setOnClickListener {
+            val goDetail = Intent(it.context, ItemDetailActivity::class.java)
+            it.context.startActivity(goDetail)
+        }
+    }
+}
--- app/src/main/java/com/example/animallist/MainActivity.kt
+++ app/src/main/java/com/example/animallist/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val animals: List<String> = listOf("Zebra", "Lion", "Giraffe")
+        val recycler: RecyclerView = findViewById(R.id.item_list)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = AnimalListAdapter(animals)
     }
 }
