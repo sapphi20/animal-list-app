c373fa47cd0e3630f572ba5e9791fd5588f0a02c
Alejandra Alarcón <alejandra.alarcon@ug.uchile.cl>
2024-09-27 01:13:14 -0300

    :sparkles: Get info from intent extras

--- app/src/main/java/com/example/animallist/AnimalInfoActivity.kt
+++ app/src/main/java/com/example/animallist/AnimalInfoActivity.kt
@@ -2,13 +2,35 @@ package com.example.animallist
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class AnimalInfoActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.animal_info)
+        val animal = intent.extras?.keySet()?.toList()?.get(0)
+        val data = intent.extras?.getStringArray(animal)?.toList()
+        if (data != null) {
+            for (d in data){
+                Log.d("asdf", d)
+            }
+        }
+
+        val title = findViewById<TextView>(R.id.detail_title)
+        val habitat = findViewById<TextView>(R.id.habitat)
+        val lifespan = findViewById<TextView>(R.id.lifespan)
+        val asdf = findViewById<TextView>(R.id.asdf)
+        title.text = animal
+        data.let {
+            habitat.text = "Habitat: " + it?.get(0)
+            lifespan.text = "Lifespan: " + it?.get(1)
+            asdf.text = it?.get(2) ?: "asdf"
+        }
+
         findViewById<Button>(R.id.back_btn).setOnClickListener {
             finish()
             Toast.makeText(this, "Going back!", Toast.LENGTH_SHORT).show()
